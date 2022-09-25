db0fd169f6df2245f7e407acaaf4f843741ecf3c
Simon Gustafsson <simongustafsson2002@gmail.com>
2022-09-25 21:24:19 +0200

    Testing traversing directories

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::{env, path::Path};
+use std::env;
+use std::fs;
+use std::path::Path;
 
 // make sure that all print statments except the result goes
 // to the standard error eprintln!()
 
 fn main() {
 
-    
+    let dir = Path::new("C:/Users/simon");
 
-    eprintln!("Hello Wolrds!");
-    println!("C:/Users/simon");
+    let entries = fs::read_dir(dir).unwrap();
+    
+    for (n, entry) in entries.enumerate() {
+        println!("{}  :  {:?}", n, entry);
+    }
 }
