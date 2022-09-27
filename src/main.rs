8b2178962975323db6210cf43da12d5075a7e8de
S1monG <si5564gu-s@student.lu.se>
2022-09-27 22:03:53 +0200

    Add functions to handle more than one directory and run cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-use std::env;
-use std::fs;
-use std::path::Path;
 use std::path::PathBuf;
-use std::process::Command;
 
 use clap::Parser;
-use clap::builder::ArgAction::SetTrue;
 
 use crate::traversal::get_subdirs_with_name;
 
 mod traversal;
 
-#[derive(Parser,Default,Debug)]
+#[derive(Parser, Default, Debug)]
 #[clap(author = "Simon Gustafsson", version, about)]
 /// Improved version of the command cd
 struct Args {
@@ -24,7 +19,7 @@ struct Args {
 
     #[clap(short, long, parse(from_occurrences))]
     /// Automaticly chose the closest directory if there are multiple directories with the same name
-    closest: usize, 
+    closest: usize,
 
     #[clap(short, long, parse(from_occurrences))]
     /// Searches from the root if true
@@ -32,23 +27,25 @@ struct Args {
 }
 
 fn main() {
-
     let args = Args::parse();
     let max_depth = if args.max_depth.is_some() {
         args.max_depth.unwrap()
     } else {
         usize::max_value()
     };
-    let dirs = get_subdirs_with_name(&args.file_name, &std::env::current_dir().unwrap(), max_depth);
+    let dirs = get_subdirs_with_name(
+        &args.file_name,
+        &std::env::current_dir().unwrap(),
+        max_depth,
+    );
 
-    if dirs.len() == 0 {
+    if dirs.is_empty() {
         no_dirs(args)
     } else if dirs.len() == 1 {
         one_dir(dirs);
     } else {
-        more_than_one_dir(dirs);
+        more_than_one_dir(dirs, &args.file_name);
     }
-
 }
 
 fn no_dirs(args: Args) {
@@ -60,6 +57,41 @@ fn one_dir(dirs: Vec<(PathBuf, usize)>) {
     println!("{}", path_name);
 }
 
-fn more_than_one_dir(dirs: Vec<(PathBuf, usize)>) {
-    
+fn more_than_one_dir(dirs: Vec<(PathBuf, usize)>, file_name: &str) {
+    if dirs.len() > 10 {
+        eprintln!(
+            "There are {} directories with name '{}'",
+            dirs.len(),
+            file_name
+        );
+        eprintln!("Do you want to see all? Y/N");
+        let input = parse_input();
+        if input.trim().to_lowercase() != "y" {
+            std::process::exit(0);
+        }
+    }
+
+    eprintln!("Choose which directory you want to cd to");
+
+    list_dirs(&dirs);
+    let input = parse_input();
+    let n: usize = input.trim().parse().unwrap();
+    if n <= dirs.len() {
+        let path_name = dirs.get(n).unwrap().0.to_str().unwrap();
+        println!("{}", path_name);
+    } else {
+        eprintln!("Outside of range");
+    }
+}
+
+fn list_dirs(dirs: &[(PathBuf, usize)]) {
+    for (i, dir) in dirs.iter().enumerate() {
+        eprintln!("[{}] : {}", i, dir.0.to_str().unwrap());
+    }
+}
+
+fn parse_input() -> String {
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).unwrap();
+    buffer
 }
--- src/traversal.rs
+++ src/traversal.rs
@@ -1,14 +1,17 @@
 use std::collections::VecDeque;
 use std::fs;
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 
-pub fn get_subdirs_with_name(file_name: &str, start: &Path, max_depth: usize) -> Vec<(PathBuf, usize)> {
+pub fn get_subdirs_with_name(
+    file_name: &str,
+    start: &Path,
+    max_depth: usize,
+) -> Vec<(PathBuf, usize)> {
     let mut paths = Vec::new();
     let mut queue = VecDeque::new();
     // start with current dir
     queue.push_back((PathBuf::from(start), 0));
     loop {
-
         if queue.is_empty() {
             break;
         }
@@ -20,14 +23,14 @@ pub fn get_subdirs_with_name(file_name: &str, start: &Path, max_depth: usize) ->
 
         let dirs = fs::read_dir(entry);
         //skip directories where access is denied
-        if dirs.is_ok() {
-            for dir in dirs.unwrap() {
+        if let Ok(d) = dirs {
+            for dir in d {
                 let dir = dir.unwrap();
                 if dir.file_type().unwrap().is_dir() {
                     if dir.file_name() == file_name {
-                        paths.push((dir.path(), depth+1));
+                        paths.push((dir.path(), depth + 1));
                     }
-                    queue.push_back((dir.path(), depth+1));
+                    queue.push_back((dir.path(), depth + 1));
                 }
             }
         }
